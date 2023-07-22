c16253c304ecf751c44443d71e740da013e876ee
M-Munim <107766393+M-Munim@users.noreply.github.com>
2023-07-23 00:33:12 +0500

    Add files via upload

---
+++ New folder/static/script.js
@@ -0,0 +1,6 @@
+// let checked = document.querySelector(".tic")
+
+document.getElementById("click").addEventListener("click", function () {
+    document.getElementById("tic").classList.toggle("tic")
+    // document.getElementById("click").innerHTML = "Added To Cart"
+})
